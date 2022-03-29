ea84f1f11b58ef0e9c18abb78f4003312140ffd6
M3R14M <meriamvangalen@gmail.com>
2022-03-29 21:06:38 +0200

    Fix case sensitivity playerSelection

--- foundations/javascript/rock-paper-scissors/script.js
+++ foundations/javascript/rock-paper-scissors/script.js
@@ -1,5 +1,5 @@
-let playerSelection = "Paper";
-const rpsArray = ["Rock", "Paper", "Scissors"];
+let playerSelection = lowerCase("Paper");
+const rpsArray = ["rock", "paper", "scissors"];
 let computerSelection = computerPlay();
 
 function computerPlay() {
@@ -7,14 +7,23 @@ function computerPlay() {
     return rpsArray[Math.floor(Math.random() * rpsArray.length)];
 }
 
+// To solve for case sensitivity without losing capitalization on results.
+function lowerCase(text) {
+    return text.toLowerCase();
+}
+
+function sentenceCase(text) {
+    return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
+}
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "Rock" && computerSelection === "Paper" ||
-        playerSelection === "Paper" && computerSelection === "Scissors" ||
-        playerSelection === "Scissors" && computerSelection === "Rock") {
+    if (playerSelection === "rock" && computerSelection === "paper" ||
+        playerSelection === "paper" && computerSelection === "scissors" ||
+        playerSelection === "scissors" && computerSelection === "rock") {
         return loseRound();
-    } else if (playerSelection === "Paper" && computerSelection === "Rock" ||
-        playerSelection === "Scissors" && computerSelection === "Paper" ||
-        playerSelection === "Rock" && computerSelection === "Scissors") {
+    } else if (playerSelection === "paper" && computerSelection === "rock" ||
+        playerSelection === "scissors" && computerSelection === "paper" ||
+        playerSelection === "rock" && computerSelection === "scissors") {
         return winRound();
     } else if (playerSelection === computerSelection) {
         return tieRound();
@@ -25,15 +34,16 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function winRound() {
-    return alert(`You win! ` + `${playerSelection} beats ${computerSelection}`);
+    return alert(`You win! ` + sentenceCase(`${playerSelection}`) + ` beats ` + sentenceCase(`${computerSelection}`));
 }
 
 function loseRound() {
-    return alert(`You lose! ` + `${computerSelection} beats ${playerSelection}`);
+    return alert(`You lose! ` + sentenceCase(`${computerSelection}`) + ` beats ` + sentenceCase(`${playerSelection}`));
 }
 
 function tieRound() {
-    return alert(`It's a tie! You both had ${computerSelection}`);
+    return alert(`It's a tie! You both had ` + sentenceCase(`${computerSelection}`));
 }
+
 
 // To play: console.log(playRound(playerSelection, computerSelection));
