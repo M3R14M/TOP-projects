b4ec3ba330387eb20ff2df29d89a2a33bcf04c0f
M3R14M <meriamvangalen@gmail.com>
2022-03-29 19:54:07 +0200

    Add working game
    Case sensitive values for playerSelection are Rock, Paper and Scissor.

--- foundations/javascript/rock-paper-scissors/script.js
+++ foundations/javascript/rock-paper-scissors/script.js
@@ -1,7 +1,39 @@
+let playerSelection = "Paper";
 const rpsArray = ["Rock", "Paper", "Scissors"];
-
+let computerSelection = computerPlay();
 
 function computerPlay() {
 
     return rpsArray[Math.floor(Math.random() * rpsArray.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Paper" ||
+        playerSelection === "Paper" && computerSelection === "Scissors" ||
+        playerSelection === "Scissors" && computerSelection === "Rock") {
+        return loseRound();
+    } else if (playerSelection === "Paper" && computerSelection === "Rock" ||
+        playerSelection === "Scissors" && computerSelection === "Paper" ||
+        playerSelection === "Rock" && computerSelection === "Scissors") {
+        return winRound();
+    } else if (playerSelection === computerSelection) {
+        return tieRound();
+    } else {
+        alert("Something went wrong. Oops.")
+    }
+
+}
+
+function winRound() {
+    return alert(`You win! ` + `${playerSelection} beats ${computerSelection}`);
+}
+
+function loseRound() {
+    return alert(`You lose! ` + `${computerSelection} beats ${playerSelection}`);
+}
+
+function tieRound() {
+    return alert(`It's a tie! You both had ${computerSelection}`);
+}
+
+// To play: console.log(playRound(playerSelection, computerSelection));
