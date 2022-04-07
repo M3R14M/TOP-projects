9604ba448ce43a24c5296587ef143387a33f313f
M3R14M <meriamvangalen@gmail.com>
2022-04-07 23:15:38 +0200

    Adjust code for UI

--- foundations/javascript/rock-paper-scissors/script.js
+++ foundations/javascript/rock-paper-scissors/script.js
@@ -2,48 +2,83 @@ let playerScore = 0;
 let computerScore = 0;
 let playerSelection;
 let computerSelection;
-let playedRounds;
+let playedRounds = 0;
 let rpsArray = ['rock', 'paper', 'scissors'];
 
 
-function computerPlay() {
-    return rpsArray[Math.floor(Math.random() * rpsArray.length)];
-}
+
+// == Button functions ==
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    playerSelection = 'rock';
+    if (playedRounds === 0) {
+        game();
+    } else {
+        computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+        winnerDeclaration();
+    }
+    return;
+});
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    playerSelection = 'paper';
+    if (playedRounds === 0) {
+        game();
+    } else {
+        computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+        winnerDeclaration();
+    }
+    return;
+});
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    playerSelection = 'scissors';
+    if (playedRounds === 0) {
+        game();
+    } else {
+        computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+        winnerDeclaration();
+    }
+    return;
+});
+
+// ==  General User Interface ==
+
+const pInput = document.getElementById('playerInput');
+const nGame = document.getElementById('newGame');
+
+// Scoreboard 
+const pScore = document.getElementById('playerScore');
+pScore.textContent = `0`;
+const cScore =
+    document.getElementById('computerScore');
+cScore.textContent = `0`;
+
+// Displaying results
+const rResult = document.getElementById('roundResult');
+const wResult = document.getElementById('winnerResult');
+
 // To solve for case sensitivity without losing capitalization on results.
 function sentenceCase(text) {
     return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
 }
 
-function playerPlay() {
-    let playerInput = prompt('Choose wisely: Rock, paper or scissors?');
-    return playerInput.toLowerCase();
+// == Game ==
+function computerPlay() {
+    return rpsArray[Math.floor(Math.random() * rpsArray.length)];
 }
 
-
 function game() {
+    playerScore = 0;
+    computerScore = 0;
     playedRounds = 0;
-    let winnerScore = playerScore || computerScore;
-    while (playedRounds < 5) {
+    if (playerScore <= 5 && computerScore <= 5) {
         computerSelection = computerPlay();
-
-        computerPlay();
-        if (computerSelection === 'rock' || 'paper' || 'scissors') {
-            playerSelection = playerPlay();
-
-            if (playerSelection === 'rock' || 'paper' || 'scissors') {
-                playRound(playerSelection, computerSelection);
-            }
-        }
-        console.log(`Current score: Player: ${playerScore} Computer: ${computerScore}`);
-        continue;
+        playRound(playerSelection, computerSelection);
     }
-    if (playerScore > computerScore) {
-        alert(`You won! You scored ${playerScore}, computer scored ${computerScore}.`)
-    } else if (computerScore > playerScore) {
-        alert(`You lost! Computer scored ${computerScore}, you scored ${playerScore}.`)
-    }
-    console.log(`Final score: Player: ${playerScore} Computer: ${computerScore}`);
-    return console.log("To play another game, type 'newGame()' in console");
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -51,6 +86,7 @@ function playRound(playerSelection, computerSelection) {
         playerSelection === 'paper' && computerSelection === 'scissors' ||
         playerSelection === 'scissors' && computerSelection === 'rock') {
         computerScore++;
+        cScore.textContent = `${computerScore}`;
         playedRounds += 1;
         loseRound();
         return;
@@ -58,38 +94,56 @@ function playRound(playerSelection, computerSelection) {
         playerSelection === 'scissors' && computerSelection === 'paper' ||
         playerSelection === 'rock' && computerSelection === 'scissors') {
         playerScore++;
+        pScore.textContent = `${playerScore}`;
         playedRounds += 1;
         winRound();
-
     } else if (playerSelection === computerSelection) {
+        playedRounds += 1;
         tieRound();
-
     } else {
         alert(`Something went wrong. Oops. Your input was: "${playerSelection}"`);
     }
 }
 
 function tieRound() {
-    console.log(`It's a tie! You both had ${sentenceCase(`${computerSelection}`)}`);
+    if (rResult.hasAttribute("class") == true) {
+        rResult.removeAttribute("class");
+    }
+    rResult.textContent = `It's a tie! You both had ${sentenceCase(`${computerSelection}`)}`;
     return;
 }
 
 function winRound() {
-    console.log(`You win! ${sentenceCase(`${playerSelection}`)} beats ${sentenceCase(`${computerSelection}`)}`);
+    rResult.className = "win";
+    rResult.textContent = `You win! ${sentenceCase(`${playerSelection}`)} beats ${sentenceCase(`${computerSelection}`)}`;
     return;
 }
 
 function loseRound() {
-    console.log(`You lose! ${sentenceCase(`${computerSelection}`)} beats ${sentenceCase(`${playerSelection}`)}`);
+    rResult.className = "lose";
+    rResult.textContent = `You lose! ${sentenceCase(`${computerSelection}`)} beats ${sentenceCase(`${playerSelection}`)}`;
     return;
 }
-alert("This game's score is kept in console. Please make sure you have opened dev tools");
-game();
+
+function winnerDeclaration() {
+    if (playerScore === 5) {
+        wResult.className = "win";
+        wResult.textContent = `Nailed it!`;
+        pInput.style.display = "none";
+        nGame.style.display = "block";
+    } else if (computerScore === 5) {
+        wResult.className = "lose";
+        wResult.textContent = `Oh no, you failed!`;
+        pInput.style.display = "none";
+        nGame.style.display = "block";
+    } else {
+        return;
+    }
+}
 
 // Start a new game
-let newGame = function () {
-    playedRounds = 0;
-    playerScore = 0;
-    computerScore = 0;
-    game();
+nGame.addEventListener("click", newGame);
+
+function newGame() {
+    window.location.reload();
 }
