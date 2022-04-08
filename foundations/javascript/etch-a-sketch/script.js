7a5d495ef35c356b51d13ac7f6faccdb67dba915
M3R14M <meriamvangalen@gmail.com>
2022-04-08 18:24:47 +0200

    Fill grid

--- foundations/javascript/etch-a-sketch/script.js
+++ foundations/javascript/etch-a-sketch/script.js
@@ -1,1 +1,12 @@
 const grid = document.getElementById("grid");
+let box;
+let amount = 16;
+let sizeGrid = Math.pow(amount, 2);
+
+function fill(sizeGrid) {
+    for (let i = 0; i < sizeGrid; i++) {
+        box = document.createElement("div");
+        grid.append(box);
+    }
+}
+fill(sizeGrid);
