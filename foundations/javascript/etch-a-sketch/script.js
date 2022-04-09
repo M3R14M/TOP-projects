d2aa484cd320bfabccc504113274b38003149c94
M3R14M <meriamvangalen@gmail.com>
2022-04-09 20:41:12 +0200

    Create clear grid function

--- foundations/javascript/etch-a-sketch/script.js
+++ foundations/javascript/etch-a-sketch/script.js
@@ -14,4 +14,21 @@ function createGrid(sizeInput) {
 function draw() {
     this.className = 'solid';
 }
+// Button for clearing current grid and creating a new grid
+let btn = document.getElementById('clear');
+btn.addEventListener('click', clear);
+
+function clear() {
+    sizeInput = prompt('What width should your grid have?');
+    if (sizeInput > 64) {
+        sizeInput = prompt('The maximum allowed size is 64.');
+    } else if (sizeInput < 4) {
+        sizeInput = prompt('A minimum of 4 is required to create a grid');
+    }
+    grid.textContent = '';
+    grid.style.gridTemplateColumns = `repeat(${sizeInput}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${sizeInput}, 1fr)`;
+    createGrid(sizeInput);
+}
+
 createGrid(16);
